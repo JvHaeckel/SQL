b59e8acee0eb4a8b29bbb5322a49dfc17fccf6a9
JvHaeckel <joaohaeckel@gmail.com>
2025-07-11 08:28:42 -0300

    MOBI

---
+++ Exibir linhas com Abastecimento Duplicado.sql
@@ -0,0 +1,12 @@
+-- Exibir linhas com Abastecimento Duplicado
+/* Ele só fez até dia 29 mas meu BI mostra que teve mais 3 casos*/
+SELECT 
+    DATA, 
+    PREFIXO, 
+    LITROS, 
+    COUNT(*) AS QTD
+FROM fact_vwpbi_abastecimento_detalhado
+WHERE DATA >= '2025-01-01 00:00:00' and DATA < '2026-01-01 00:00:00'
+GROUP BY DATA, PREFIXO, LITROS
+HAVING QTD > 1
+ORDER BY PREFIXO, DATA, LITROS;
