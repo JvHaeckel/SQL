e5f13f49f27fa3b9139946a5bc9947a130c7fad4
JvHaeckel <joaohaeckel@gmail.com>
2025-07-15 11:11:32 -0300

    MOBI

--- Colunas importantes da tabela Abastecimento Detalhado.sql
+++ Colunas importantes da tabela Abastecimento Detalhado.sql
@@ -1,6 +1,7 @@
-/* Colunas importantes da tabela Abastecimento Detalhado*/
+-- Colunas importantes da tabela Abastecimento Detalhado
+
 SELECT EMPRESA, DATA,PREFIXO, HODINICIALVELOC, HODFINALVELOC, KMPERCORRIDO, 
 LITROS, CONSUMO, TIPO_FROTA, VALOR, LINHA
-FROM fact_vwpbi_abastecimento_detalhado where data >= '2025-01-01 00:00:00'
+FROM fact_vwpbi_abastecimento_detalhado WHERE DATA >= '2025-01-01 00:00:00' 
 
  
