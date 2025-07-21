92e5e9f44b96344590f557c430a564500b1ced64
JvHaeckel <joaohaeckel@gmail.com>
2025-07-21 08:08:31 -0300

    MOBI

--- Abastecimento sem rodar possível falha de integração ou leitura.sql
+++ Abastecimento sem rodar possível falha de integração ou leitura.sql
@@ -1,4 +1,4 @@
--- veículos abastecendo sem rodar possível falha de integração ou leitura
+-- Abastecimento sem rodar possível falha de integração ou leitura
 SELECT 
   EMPRESA,  PREFIXO,
   COUNT(*) AS Abastecimentos_KmZero, /* Quantas vezes o veículo abasteceu sem rodar*/
@@ -7,6 +7,6 @@ SELECT
                                     /* MAX(DATA) → última vez que isso ocorreu */
 FROM fact_vwpbi_abastecimento_detalhado WHERE DATA >= '2025-01-01 00:00:00' AND KMPERCORRIDO = 0
 GROUP BY EMPRESA,  PREFIXO
-HAVING COUNT(*) >= 2     /* Só quero ver os veículos que abasteceram com KM = 0 2 ou mais vezes" */
+HAVING COUNT(*) >= 2     /* Só quero ver os veículos que abasteceram com KM = 0 em 2 ou mais vezes" */
 
 ORDER BY Abastecimentos_KmZero DESC;
