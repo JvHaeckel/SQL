52c923a1e668b37840943e5e93fb7cdbf35587c7
JvHaeckel <joaohaeckel@gmail.com>
2025-07-31 17:43:22 -0300

    MOBI

---
+++ Gt Frotas/Km Regressivo/Tabela Km Regressivo por Tipo Frota.sql
@@ -0,0 +1,8 @@
+-- Tabela Km Regressivo por Tipo Frota Gt Frotas
+
+SELECT   
+(HAS_ODOMETRO - KM_ANT) AS RecalculaKm 
+ FROM fact_vwpbi_abast_gtfrota  WHERE HAS_DATA >= '2025-01-01 00:00:00' AND 
+ (HAS_ODOMETRO - KM_ANT) < 0
+ 
+ ORDER BY HAS_DATA ASC
