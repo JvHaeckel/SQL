9971d4f05edf788ecaed9832f1149225056aef06
JvHaeckel <joaohaeckel@gmail.com>
2025-08-01 09:43:21 -0300

    MOBI

---
+++ Gt Frotas/Volume Excedente/Inconsistência na quantidade de Litros(HAS_QTD_LITROS).sql
@@ -0,0 +1,26 @@
+/*Inconsistência na quantidade de Litros(HAS_QTD_LITROS)
+
+Abri o chamado: C2507-353 no link: 
+https://mobibrasil.topdesk.net/tas/public/ssp/content/detail/incident?unid=f86a79fc-40ad-4270-9bc5-511a158537ab
+
+Inconsistência na quantidade de Litros(HAS_QTD_LITROS), ao fazer a diferença entre os encerrantes para checar 
+os dados percebi que alguns valores não batem. Como podemos ver abaixo existem uma série de desvios:  */
+
+
+SELECT * FROM (
+  SELECT 
+   HAS_DATA, EMPRESA, VEI_IDENTIFICACAO_EMPRESA, HAS_QTD_LITROS, 
+ (HAS_ENCERRANTE_DEPOIS - HAS_ENCERRANTE_ANTES) AS RECALCULA_QTD,
+
+    CASE 
+WHEN (HAS_ENCERRANTE_DEPOIS - HAS_ENCERRANTE_ANTES) = HAS_QTD_LITROS THEN 'OK' ELSE 'ERRO' 
+    END AS CheckList,
+
+VEI_LIMITE_ABASTECIMENTO,(HAS_ODOMETRO - KM_ANT ) AS RECALCULA_KM,HAS_ENCERRANTE_ANTES, 
+ HAS_ENCERRANTE_DEPOIS, HAS_ODOMETRO, KM_ANT, KM_PERCO
+
+  FROM fact_vwpbi_abast_gtfrota WHERE HAS_DATA >= '2025-01-01 00:00:00'
+) AS sub
+
+WHERE CheckList = 'ERRO' AND NOT (
+HAS_QTD_LITROS = 0 AND (HAS_ENCERRANTE_DEPOIS - HAS_ENCERRANTE_ANTES) IS NULL)
---
+++ Gt Frotas/Volume Excedente/Tabela Volume Excedente com Litros calculados.sql
@@ -0,0 +1,17 @@
+-- Tabela Volume Excedente com Litros calculados
+SELECT 
+  EMPRESA, HAS_DATA AS DATA, VEI_IDENTIFICACAO_EMPRESA AS Prefixo, 
+  VEI_LIMITE_ABASTECIMENTO AS Tanque, HAS_QTD_LITROS AS Litros,
+  ROUND(HAS_ENCERRANTE_DEPOIS - HAS_ENCERRANTE_ANTES,2) AS NewLitros ,
+ 
+  -- Volume que excede o limite permitido
+   ROUND((HAS_ENCERRANTE_DEPOIS - HAS_ENCERRANTE_ANTES) - (VEI_LIMITE_ABASTECIMENTO),2 ) AS Volume_Excedente
+
+FROM fact_vwpbi_abast_gtfrota
+WHERE 
+  HAS_DATA >= '2025-01-01 00:00:00'
+  AND ROUND(HAS_ENCERRANTE_DEPOIS - HAS_ENCERRANTE_ANTES,2)  > VEI_LIMITE_ABASTECIMENTO  -- Só mostra quando há excesso
+
+ORDER BY 
+  HAS_DATA ASC;  
+
