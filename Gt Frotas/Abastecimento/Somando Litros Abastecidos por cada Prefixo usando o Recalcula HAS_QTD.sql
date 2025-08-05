345df3f764bacbf4d6031362216b533cacfc0ea3
JvHaeckel <joaohaeckel@gmail.com>
2025-08-05 14:25:09 -0300

    MOBI

--- Gt Frotas/Abastecimento/Somando Litros Abastecidos por cada Prefixo usando o Recalcula HAS_QTD.sql
+++ Gt Frotas/Abastecimento/Somando Litros Abastecidos por cada Prefixo usando o Recalcula HAS_QTD.sql
@@ -6,8 +6,10 @@ ROUND(SUM(HAS_ENCERRANTE_DEPOIS - HAS_ENCERRANTE_ANTES),2) AS RecalculaLitros
  FROM fact_vwpbi_abast_gtfrota WHERE HAS_DATA >= '2025-01-01 00:00:00' 
  
  GROUP BY VEI_IDENTIFICACAO_EMPRESA  -- Ao colocar GROUP BY já ajusta para somar a cada prefixo
+ ORDER BY RecalculaLitros DESC
  
- 
- /* Prefixo Mais Abastecido: 0063032
-    Prefixo Menos Abastecido: 0006340
+ /* Prefixo Mais Abastecido: 0063032 - 42157.24
+    Prefixo Menos Abastecido: 
+	NULL  - 0063798 0063802 1064607 0064644	
+    56.21 - 0063016	
 */
