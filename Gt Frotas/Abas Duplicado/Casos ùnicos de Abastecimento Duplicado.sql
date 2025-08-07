11f55db9c528aa28c57b9213683704cdfa40b409
JvHaeckel <joaohaeckel@gmail.com>
2025-08-07 09:18:57 -0300

    MOBI

--- Gt Frotas/Abas Duplicado/Casos ùnicos de Abastecimento Duplicado.sql
+++ Gt Frotas/Abas Duplicado/Casos ùnicos de Abastecimento Duplicado.sql
@@ -1,14 +1,13 @@
--- Casos ùnicos de Abastecimento Duplicado
-
+-- Casos únicos de Abastecimento Duplicado
 SELECT 
   COUNT(*) AS QTD_Abas_Duplicado
 FROM (
   SELECT 
-    DATE(DATA) AS DATA_SEM_HORA,
-    PREFIXO,
-    LITROS
-  FROM fact_vwpbi_abastecimento_detalhado
-  WHERE DATA >= '2025-01-01 00:00:00' 
-  GROUP BY DATE(DATA), PREFIXO, LITROS
+    DATE(HAS_DATA) AS Data, 
+    VEI_IDENTIFICACAO_EMPRESA AS Prefixo, 
+    HAS_QTD_LITROS AS LITROS
+  FROM fact_vwpbi_abast_gtfrota  WHERE HAS_DATA >= '2025-01-01 00:00:00' 
+  GROUP BY DATE(HAS_DATA), VEI_IDENTIFICACAO_EMPRESA, HAS_QTD_LITROS
   HAVING COUNT(*) >= 2
 ) AS GruposDuplicados;
+
