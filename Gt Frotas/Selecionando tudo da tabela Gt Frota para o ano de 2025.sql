a527e0c7fba4a8243bd26071ca47216eaee56d0f
JvHaeckel <joaohaeckel@gmail.com>
2025-08-07 15:44:13 -0300

    MOBI

--- Gt Frotas/Selecionando tudo da tabela Gt Frota para o ano de 2025.sql
+++ Gt Frotas/Selecionando tudo da tabela Gt Frota para o ano de 2025.sql
@@ -1,5 +1,2 @@
 -- Selecionando tudo da tabela Gt Frota para o ano de 2025
-SELECT * FROM fact_vwpbi_abast_gtfrota where HAS_DATA >= '01-01-2025' and HAS_DATA < '01-01-2026';
-
-SELECT * FROM fact_vwpbi_abast_gtfrota
-WHERE HAS_DATA >= '2025-01-01' AND HAS_DATA < '2026-01-01';
+SELECT * FROM fact_vwpbi_abast_gtfrota where HAS_DATA >= '2025-01-01 00:00:00' and HAS_DATA < '2026-01-01  00:00:00';
---
+++ Tanques.sql
@@ -0,0 +1,6 @@
+-- 	Tanques
+SELECT 
+DISTINCT VEI_LIMITE_ABASTECIMENTO
+
+ FROM fact_vwpbi_abast_gtfrota where HAS_DATA >= '2025-01-01 00:00:00' and HAS_DATA < '2026-01-01  00:00:00';
+GROUP BY 
